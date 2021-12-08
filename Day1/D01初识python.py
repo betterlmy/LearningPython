2b07d10df8c3a3d6260dfc0a515be8f6782bc2c4
Zane <betterlmy@icloud.com>
2021-12-08 22:39:45 +0800

    Upload Day1's content

---
+++ Day1/D01初识python.py
@@ -0,0 +1,19 @@
+# author:TYUT-Lmy
+# date:2021/12/8
+# description:第一天
+print(1)
+import this #输出python之禅
+import turtle
+# turtle 绘制图形
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
